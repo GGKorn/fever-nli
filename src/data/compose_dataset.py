16132b13a86fa2e9bb356628d1c1b759827e2f2d
HeuteMalAnders <benni@henne-clan.de>
2019-01-22 20:24:02 +0100

    Decluttered and annotated decompose_dataset.py method stump

--- src/data/compose_dataset.py
+++ src/data/compose_dataset.py
@@ -1,21 +1,10 @@
 import jsonlines
 import os
-import pprint
 
-# depending on number of sentence_id digits the extraction needs to begin at +1 cells to compensate
-def extract_sentence(sentence_id, wiki_data, claim):
-    if not wiki_data['id']:
-        raise RuntimeError("extract_sentence() received call to read {} from empty wiki_data".format(sentence_id))
-    sentence_start = wiki_data['lines'].find("{}\t".format(sentence_id)) + 2
-    sentence_end = wiki_data['lines'].find(".\t", sentence_start) + 1
-    print(sentence_start, sentence_end)
-    if sentence_start == -1 or sentence_end == -1:
-        raise RuntimeError("sentence_id {} not found for wiki_id {}".format(sentence_id, wiki_data['id']))
-    else:
-        return wiki_data['lines'][sentence_start:sentence_end]
-
+# path to wiki-dump files
 path_to_data = r"E:/Data/wikipedia_dump_2017/wiki-pages/"
-json_files = [fname for fname in os.listdir(path_to_data) if fname.endswith('.jsonl')]
+# collect the names of all 
+json_files = [fname for fname in os.listdir(path_to_data) if fname.startswith('wiki-') and fname.endswith('.jsonl')]
 
 wiki_data = list()
 for i,f in enumerate(json_files):
@@ -23,20 +12,13 @@ for i,f in enumerate(json_files):
         for itm in jreader:
             wiki_data.append(itm)
         print("Parsing {} of {} data chunks. Total entries: {}".format(i+1, len(json_files), len(wiki_data)))
-        break
 
-pp = pprint.PrettyPrinter(indent=4)
-# wiki_data[2]['testfield'] = "test"
-# pp.pprint(wiki_data[2])
-pp.pprint(extract_sentence(13, wiki_data[2], ""))
-
-# with jsonlines.open('./data/train.jsonl') as jreader:
-#     for itm in jreader:
-#         print(itm)
-#         break
-
-# with open('./data/train.jsonl', 'r') as f:
-#     res = [json.loads(jline) for jline in f]
-
-# pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(res[:10])
+with jsonlines.open('./data/train.jsonl') as jreader:
+    for itm in jreader:
+        print("itm: ", itm)
+        for evidence in itm['evidence']:
+            for _, eid, name, sid in evidence:
+                # for every claim/label pair in train.jsonl, corresponding evidence sentence needs to be found in wiki_data
+                # evidence name is the dict-key to the wiki page in wiki_data, util/misc.py::extract_sentence can be
+                # used to extract extract sentences from the corpus
+                print("evidence_id:(", eid, ") name:(", name, ") sentence_id:(", sid, ")") 
