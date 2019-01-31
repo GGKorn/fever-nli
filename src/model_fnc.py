a632ebb68476c8000f8b91d1dd8c9f1289e53d4d
HeuteMalAnders <benni@henne-clan.de>
2019-01-31 20:00:57 +0100

    Base implementation of the FNC model

---
+++ src/model_fnc.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+
+class SimpleBaselineModel(object):
+    """Implementation of single hidden-layer FFN model of the FEVER baseline."""
+    def __init__(self, features, labels, mode, hparams):
+        """Initialises the model with all components."""
+        self.hparams = hparams
+        # fixed hyperparameters that will not be available via commandline
+        self.hidden_units = 100
+        self.l2_lambda = 0.00001
+        self.clip_gradients = True
+        self.clip_norm = 5
+
+        with tf.name_scope('inputs'):
+            self.inputs = features
+            self.labels = labels
+            self.learning_rate = hparams.learning_rate
+            self.global_step = tf.Variable(0, trainable=False, name='global_step')
+
+        self._build_model(mode)
+
+    def _build_model(self, mode):
+        """
+        Builds the model and prepares return values for EstimatorSpecs.
+
+        Parameters:
+            mode:   tf.estimator.ModeKeys value [TRAIN, EVAL, PREDICT], denotes purpose of current run
+        """
+        with tf.variable_scope('hidden_layer'):
+            # hidden layer
+            model = tf.layers.dense(self.inputs, 
+                                    self.hidden_units,
+                                    activation=tf.nn.relu,
+                                    kernel_initializer=tf.keras.initializers.he_uniform(),
+                                    name='dense01')
+            # 1st dropout
+            model = tf.nn.dropout(model, self.hparams.dropout_keep_prob)
+        
+        with tf.variable_scope('output_layer'):
+            # output layer
+            model = tf.layers.dense(model,
+                                    self.hparams.logit_dims,
+                                    activation=None,
+                                    name='logits')
+            # 2nd dropout, god knows why...
+            model = tf.nn.dropout(model, self.hparams.dropout_keep_prob)
+            self.logits = tf.reshape(model, [self.hparams.batch_size, self.hparams.logit_dims])
+
+        # objective ops
+        with tf.variable_scope('objective'):
+            if mode == tf.estimator.ModeKeys.TRAIN:
+                # gather l2 coefficients and compute penalty
+                l2_coefficients = [tf.nn.l2_loss(var) for var in tf.trainable_variables() if 'bias' not in var.name]
+                l2_penalty = tf.multiply(tf.add_n(l2_coefficients), self.l2_lambda)
+
+            # calculate base xentropy loss, add l2 penalty in case of TRAIN
+            xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(self.logits, self.labels)
+            xentropy = xentropy + l2_penalty if mode == tf.estimator.ModeKeys.TRAIN else xentropy
+            self.loss = tf.reduce_sum(xentropy, name='loss_xentropy')
+
+            # PREDICT EstimatorSpec
+            predicted_classes = tf.argmax(self.logits, 1)
+            self.predictions = {
+                'class': predicted_classes,
+                'prob': tf.nn.softmax(self.logits)
+            }
+
+            # EVAL EstimatorSpec
+            self.eval_metric_ops = {
+                'accuracy': tf.metrics.accuracy(
+                    labels=self.labels, predictions=predicted_classes)
+            }
+
+        with tf.variable_scope('optimisation'):
+            # init optimiser, compute gradients, apply clipping during TRAIN
+            optimizer = tf.train.AdamOptimizer(learning_rate=self.hparams.learning_rate)
+            gradients = optimizer.compute_gradients(self.loss, tf.trainable_variables())
+            if self.clip_gradients:
+                gradients, _ = tf.clip_by_global_norm(gradients, self.clip_norm)
+            self.train_op = optimizer.apply_gradients(gradients, global_step=tf.train.get_global_step())
