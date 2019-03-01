71035147c5e970a10266dcf63fec496b449ac38f
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2019-03-01 02:06:34 +0100

    Rearranged inputs, switched to glove embeddings, added input cutoff, added docstrings, fixed _load_fever, set input_fn output

--- src/input_da.py
+++ src/input_da.py
@@ -1,32 +1,27 @@
-import tensorflow as tf
-from ast import literal_eval
 import os
+import unicodedata
 import numpy as np
 import scipy as sp
-from glob import iglob
 import pandas as pd
-import unicodedata
+import tensorflow as tf
+from glob import iglob
+from ast import literal_eval
+from gensim.models.keyedvectors import KeyedVectors
 
-
-debug = 5000
+debug = False
 TrainingPath = "train_data_*.csv"
 EvalPath = "eval_data_*.csv"
 TestPath = "test_data_*.csv"
 
-
-from gensim.models.keyedvectors import KeyedVectors
-W2V_PATH = "/workData/generalUseData/GoogleNews-vectors-negative300.bin.gz"
-W2V_IS_BINARY = True
+# W2V_PATH = r".\embedding\gensim_glove.6B.300d.txt"
+W2V_PATH = r"E:\Python\ANLP Final Project\data\embedding\gensim_glove.6B.300d.txt"
 emb_size_threshold = 500
 emb_size = 300
-
 
 # from https://stackoverflow.com/questions/48057991/get-word-embedding-dictionary-with-glove-python-model
 # model.word_vectors[model.dictionary['samsung']]
 
-
-
-def get_input_fn(mode=None):
+def get_input_fn_da(mode=None):
     """Creates an input function that loads the dataset and prepares it for use."""
 
     def _input_fn(mode=None, params=None):
@@ -44,46 +39,56 @@ def get_input_fn(mode=None):
         #   5. pass file name to load_fever
         #   6. load the already indexed data in load_fever
 
-        vectors = KeyedVectors.load_word2vec_format(W2V_PATH, binary=W2V_IS_BINARY)
-
-
+        vectors = KeyedVectors.load_word2vec_format(W2V_PATH, binary=False)
 
         with tf.device('/cpu:0'):
             if mode == 'train':
-                ds_gen = get_dataset_generator(os.path.join(params.data_dir, TrainingPath), vectors, params.batch_size)
+                ds_gen = get_dataset_generator(os.path.join(params.data_dir, TrainingPath), vectors, params.cutoff_len, params.batch_size)
 
                 dataset = tf.data.Dataset.from_generator(
                     generator = ds_gen,
-                    output_types = (tf.float32,tf.float32, tf.int64,tf.int64,tf.int64,tf.int64)
+                    output_types = (tf.float32, tf.float32, tf.int64, tf.int64, tf.int64, tf.int64)
                 )
                 dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(buffer_size=10, count=None))
                 dataset = dataset.prefetch(buffer_size=1)
             elif mode == 'eval':
-                ds_gen = get_dataset_generator(os.path.join(params.data_dir, EvalPath), vectors, params.eval_batch_size)
+                ds_gen = get_dataset_generator(os.path.join(params.data_dir, EvalPath), vectors, params.cutoff_len, params.eval_batch_size)
                 dataset = tf.data.Dataset.from_generator(
                     generator = ds_gen,
-                    output_types = (tf.float32,tf.float32, tf.int64,tf.int64,tf.int64,tf.int64)
+                    output_types = (tf.float32, tf.float32, tf.int64, tf.int64, tf.int64, tf.int64)
                 )
-                dataset = dataset.shuffle(buffer_size=1)
+                # dataset = dataset.shuffle(buffer_size=1)
                 dataset = dataset.prefetch(buffer_size=1)
             elif mode == 'predict':
-                ds_gen = get_dataset_generator(os.path.join(params.data_dir, TestPath), vectors, params.eval_batch_size)
+                ds_gen = get_dataset_generator(os.path.join(params.data_dir, TestPath), vectors, params.cutoff_len, params.eval_batch_size)
                 dataset = tf.data.Dataset.from_generator(
                     generator = ds_gen,
-                    output_types = (tf.float32,tf.float32, tf.int64,tf.int64,tf.int64,tf.int64)
+                    output_types = (tf.float32, tf.float32, tf.int64, tf.int64, tf.int64, tf.int64)
                 )
-                dataset = dataset.shuffle(buffer_size=1)
+                # dataset = dataset.shuffle(buffer_size=1)
                 dataset = dataset.prefetch(buffer_size=1)
             else:
                 raise ValueError('_input_fn received invalid MODE')
-        return dataset.make_one_shot_iterator().get_next()
+
+            dataset = dataset.make_one_shot_iterator().get_next()
+            return (dataset[0], dataset[1], dataset[2], dataset[3]), (dataset[4], dataset[5])
 
     return _input_fn
 
 
-def get_dataset_generator(file, emb_vectors, batch_size=500):
+def get_dataset_generator(file, emb_vectors, cutoff_len, batch_size=32):
+    """
+    Creates python generator that yields preprocessed, glove-embedded, padded and cut-off batches of the dataset.
 
+    Parameters:
+        file:           filename or name pattern to locate the input files
+        emb_vectors:    word embedding vectors
+        cutoff_len:     maximum token length after which an input sentence will be cut off
+        batch_size:     number determining how many data samples get stacked together to form one batch
 
+    Returns:
+        A python generator that yields batches of the dataset.
+    """
 
     def _load_fever():
         """
@@ -91,71 +96,54 @@ def get_dataset_generator(file, emb_vectors, batch_size=500):
         :param file:
         :return:
         """
-
         claims, evidences, labels, verify_labels = get_fever_claim_evidence_pairs(file)
-        # to extract word vector
-        #lookup_em = lambda x: [emb_vectors[token] for token in x.split() if token in emb_vectors]
 
-        def lookup_em(x):
-
+        def lookup_emb(target):
             embedding_list = []
-            for token in x.split():
+            for i, token in enumerate(target.split()):
+                if i > cutoff_len:
+                    break
                 token = unicodedata.normalize("NFD",token)
                 if token in emb_vectors:
                     embedding_list.append(emb_vectors[token])
                 else:
-                    rand_embed = np.random.uniform(size=(emb_size if emb_size <= emb_size_threshold else emb_size_threshold))
+                    rand_embed = np.random.uniform(size=(emb_size))
                     embedding_list.append(rand_embed)
-            return embedding_list
+            return np.array(embedding_list).reshape(-1, emb_size)
 
-        # evid_max_len = map(lookup_em,evidences)
-        evid_max_len = 0
-        max_len_index = 0
-        for i,sent in enumerate(evidences):
-            sent_len = len(lookup_em(sent))
-            if sent_len > evid_max_len:
-                evid_max_len = sent_len
-                max_len_index = i
-        else:
-            print("longest evi",evid_max_len,unicodedata.normalize("NFD",evidences[max_len_index]))
-        #claim_max_len = map(lookup_em,claims)
-        claim_max_len = 0
-        max_len_index = 0
-        for i, sent in enumerate(claims):
-            sent_len = len(lookup_em(sent))
-            if sent_len > claim_max_len:
-                claim_max_len = sent_len
-                max_len_index = i
-        else:
-            print("longest claim",claim_max_len,claims[max_len_index])
+        evid_max_len = cutoff_len - 1
+        claim_max_len = 60
 
         batch_start = 0
         for batch_end in range(batch_size, len(claims), batch_size):
-
             emb_claims, emb_evidence = [],[]
-            emb_claim_lens, emb_evidence_lens = [],[]
-            for batch_intern_index,i in enumerate(range(batch_start,batch_end,1)):
+            emb_claim_lens, emb_evidence_lens = [], []
+            for i in range(batch_start,batch_end,1):
 
                 # get embdding for sentence
-                single_claim_emb = lookup_em(claims[i])
-                single_evidence_emb = lookup_em(evidences[i])
+                single_claim_emb = lookup_emb(claims[i])
+                single_evidence_emb = lookup_emb(evidences[i])
 
                 # fill length vector for output
                 emb_claim_lens.append(len(single_claim_emb))
                 emb_evidence_lens.append(len(single_evidence_emb))
 
+                # add zero-pad in front, also helps with empty evidences
+                # zero-pad up to max_sent_length
+                single_evidence_emb = np.pad(single_evidence_emb, ((1, evid_max_len - single_evidence_emb.shape[0]), (0, 0)), mode='constant')
+                single_claim_emb = np.pad(single_claim_emb, ((1, claim_max_len - single_claim_emb.shape[0]), (0, 0)), mode='constant')
+
                 # fill batches with embedded sentence
                 emb_claims.append(single_claim_emb)
                 emb_evidence.append(single_evidence_emb)
 
-                # add zero-pad in front, also helps with empty evidences
-                # zero-pad up to max_sent_length
-
-                emb_evidence[batch_intern_index] = np.pad(emb_evidence[batch_intern_index], (1, (evid_max_len - emb_evidence_lens[batch_intern_index])), mode='constant')
-                emb_claims[batch_intern_index] = np.pad(emb_claims[batch_intern_index], (1, (claim_max_len - emb_claim_lens[batch_intern_index])), mode='constant')
-
-            emb_evidence_lens, emb_claim_lens = np.array(emb_evidence_lens).reshape((batch_size,)), np.array(emb_claim_lens).reshape((batch_size,))
-            yield emb_claims, emb_evidence, emb_evidence_lens, emb_claim_lens, labels[batch_start:batch_end], verify_labels[batch_start:batch_end]
+            # emb_evidence_lens, emb_claim_lens = np.array(emb_evidence_lens).reshape((batch_size,)), np.array(emb_claim_lens).reshape((batch_size,))
+            yield   np.stack(emb_claims), \
+                    np.stack(emb_evidence), \
+                    np.stack(emb_evidence_lens), \
+                    np.stack(emb_claim_lens), \
+                    np.stack(labels[batch_start:batch_end]), \
+                    np.stack(verify_labels[batch_start:batch_end])
             batch_start = batch_end
 
     return _load_fever
@@ -193,23 +181,24 @@ def get_fever_claim_evidence_pairs(file_pattern,concat_evidence=True):
     return claim_list, evidence_list, label_list, verif_list
 
 
-
-
 if __name__ == "__main__":
-    Embedding_Path = None
-    local_test_path = "/workData/Uni/NLP/project/fever-nli/data/vanilla_wiki_data"
+    from timeit import default_timer as timer
+    start = timer()
+    # local_test_path = "/workData/Uni/NLP/project/fever-nli/data/vanilla_wiki_data"
+    local_test_path = r"E:\Python\ANLP Final Project\data\vanilla_wiki_data"
     if debug:
-        vectors = KeyedVectors.load_word2vec_format(W2V_PATH, binary=W2V_IS_BINARY,limit=debug)
+        vectors = KeyedVectors.load_word2vec_format(W2V_PATH, binary=False,limit=debug)
     else:
-        vectors = KeyedVectors.load_word2vec_format(W2V_PATH, binary=W2V_IS_BINARY)
-    # local_test_path = r"E:\Python\ANLP Final Project\data\vanilla_wiki_data"
-    ds_gen = get_dataset_generator(os.path.join(local_test_path,TestPath),vectors)
-    # print(ds_gen())
+        vectors = KeyedVectors.load_word2vec_format(W2V_PATH, binary=False)
+    emb = timer()
+    print('Loading embeddings took {} seconds'.format(emb - start))
+    ds_gen = get_dataset_generator(os.path.join(local_test_path,TestPath),vectors, 500)
     i = 0
     for a,b,c,d,e,f in ds_gen():
-        print(a[:2], b[:2],c.shape,d.shape)
         i += 1
-        print(i)
-        if i > 10:
+        print(i, ":", a.shape, b.shape, c.shape, d.shape, e.shape, f.shape)
+        if i > 0:
             break
     print("iterations: ", i)
+    end = timer()
+    print("Preprocessing and batching took {} seconds".format(end - emb))
