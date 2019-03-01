d1c28ce74e0854ac73bad79e4bace594168cdaa7
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2019-03-01 22:56:15 +0100

    Added more comments

--- src/input_fnc.py
+++ src/input_fnc.py
@@ -1,17 +1,14 @@
-import argparse
-
-import tensorflow as tf
-import pandas as pd
-import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer as TFIDFVec
-from sklearn.feature_extraction.text import CountVectorizer as TFVec
-from sklearn.metrics.pairwise import cosine_similarity
-from ast import literal_eval
 import os
 import scipy
+import argparse
+import numpy as np
+import pandas as pd
+import tensorflow as tf
 from glob import iglob
-
-
+from ast import literal_eval
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer as TFVec
+from sklearn.feature_extraction.text import TfidfVectorizer as TFIDFVec
 
 TrainingPath = "train_data_*.csv"
 EvalPath = "eval_data_*.csv"
@@ -41,13 +38,18 @@ def get_input_fn_fnc(mode=None):
         """
         with tf.device('/cpu:0'):
             if mode == 'train':
+                # fixed output shapes because the output will always be batches of 
+                # ([5000-dim TF vector of claims], [tf-idf cosine similary], [5000-dim TF vector of evidence])
                 ds_gen = get_dataset_generator(os.path.join(params.data_dir, TrainingPath), params.batch_size)
                 dataset = tf.data.Dataset.from_generator(
                     generator = ds_gen,
                     output_types = (tf.float32, tf.int64),
                     output_shapes = ([params.batch_size, 10001],[params.batch_size])
                 )
+                # shuffles and repeats the dataset. Relatively low shuffle buffer due to memory constraints, but randomness
+                # should nevertheless be guaranteed
                 dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(buffer_size=10, count=None))
+                # prefetching prepares another batch in case the CPU is idling during GPU computations
                 dataset = dataset.prefetch(buffer_size=1)
             elif mode == 'eval':
                 ds_gen = get_dataset_generator(os.path.join(params.data_dir, EvalPath), params.eval_batch_size)
@@ -69,6 +71,7 @@ def get_input_fn_fnc(mode=None):
                 dataset = dataset.prefetch(buffer_size=1)
             else:
                 raise ValueError('_input_fn received invalid MODE')
+        # turn dataset into an iterator, set the functions return value as the first object in the iterator
         return dataset.make_one_shot_iterator().get_next()
 
     return _input_fn
@@ -93,9 +96,11 @@ def get_dataset_generator(file=None, batch_size=500):
         """
         claims,evidences,documents,labels = get_claim_evidence_pairs(file)
 
+        # collect TF information and tf-idf vectors, garbage-collect the documents to conserve memory
         tf_vec = TFVec(stop_words="english", max_features=5000).fit(documents)
         tfidf_vec = TFIDFVec(stop_words="english", max_features=5000).fit(documents)
         del documents
+        # compute tf and tf-idf vectors for evidence and claims
         tf_claims = tf_vec.transform(claims)
         tf_evidences = tf_vec.transform(evidences)
         tfidf_claims = tfidf_vec.transform(claims)
@@ -104,6 +109,8 @@ def get_dataset_generator(file=None, batch_size=500):
         batch_start = 0
         for batch_end in range(batch_size, len(claims), batch_size):
 
+            # compute cosine similary for tf-idf vectors. Only interesting in diagonal dimension, however sklearn's implementation
+            # is much more effective/faster/cheap than manually computing only the diagonal
             tfidf_sims = np.diag(cosine_similarity(tfidf_claims[batch_start:batch_end,], tfidf_evidences[batch_start:batch_end,])).reshape(-1,1)
 
             yield scipy.sparse.hstack((tf_claims[batch_start:batch_end,], tfidf_sims, tf_evidences[batch_start:batch_end,])).A , labels[batch_start:batch_end]
@@ -126,6 +133,7 @@ def get_claim_evidence_pairs(file_pattern, concat_evidence=True):
 
     converter = {"evidence" : literal_eval}
     file_list = iglob(file_pattern)
+    # reads in all files that match the provided file pattern handle and concatenates them into one record
     data_frame = pd.concat(pd.read_csv(f, converters=converter) for f in file_list)
 
     # concatenate evidence string for documents anyway
